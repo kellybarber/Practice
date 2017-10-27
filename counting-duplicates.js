fbb99667b9d360b20acec76226fa0e8a21b363d5
kellybarber <kelly.james.barber@gmail.com>
2017-10-27 10:39:56 -0400

    counting duplicates solved

--- counting-duplicates.js
+++ counting-duplicates.js
@@ -1,6 +1,23 @@
 function duplicateCount(text){
+  let textArray     = text.toLowerCase().split('')
+  let noDuplicates  = {}
+  let numDuplicates = 0
 
+  textArray.forEach((letter) => {
+    if (!noDuplicates[letter]) {
+      noDuplicates[letter] = 1
+    } else {
+      noDuplicates[letter] += 1
+    }
+  })
 
+  Object.keys(noDuplicates).forEach((letter) => {
+    if (noDuplicates[letter] > 1) {
+      numDuplicates += 1
+    }
+  })
+
+  return numDuplicates
 
 }
 
