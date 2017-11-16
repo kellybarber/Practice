7680e5557d166bdf116ecc295c82f98bd4c3cb96
kellybarber <kelly.james.barber@gmail.com>
2017-11-16 12:09:17 -0500

    wierd case challenge needed to look at full solutions to totally complete

---
+++ unique-in-order.js
@@ -0,0 +1,6 @@
+function uniqueInOrder(iterable) {
+
+}
+
+uniqueInOrder('AAAABBBCCDAABBB')
+// ['A','B','C','D','A','B']
--- wierd-string-case.js
+++ wierd-string-case.js
@@ -1,9 +1,30 @@
 function toWeirdCase(string){
-  
+  let weirdCase = ''
 
+  string.split('').forEach((letter, index) => {
+    if (index % 2 === 0) {
+      weirdCase += letter.toUpperCase()
+    } else {
+      weirdCase += letter.toLowerCase()
+    }
+  })
+
+  return weirdCase
 
 }
 
 
+// console.log(toWeirdCase('This is a test'));
 
-toWeirdCase('This is a test')
+
+
+function toWeirdCase(string){
+  return string.split(' ').map((word) => {
+    return word.split('').map((letter, index) => {
+      return index % 2 == 0 ? letter.toUpperCase() : letter.toLowerCase()
+    }).join('');
+  }).join(' ');
+}
+
+
+console.log(toWeirdCase('This is a test'));
