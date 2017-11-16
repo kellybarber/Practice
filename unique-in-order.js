ba0352f06ca5569a8338bc8448b949f18b4dd00b
kellybarber <kelly.james.barber@gmail.com>
2017-11-16 17:04:05 -0500

    function works with letters, need to adjust to work with numbers

--- unique-in-order.js
+++ unique-in-order.js
@@ -1,10 +1,10 @@
 function uniqueInOrder(iterable) {
 
-  return iterable.split('').map((letter, index) => {
-    if ( iterable[index] === letter ) return letter 
-  })
+  return iterable.toString().split('').map((letter, index) => {
+    return iterable[index+1] !== letter ? letter : ''
+  }).filter((letter) => { return letter })
 
 }
 
 console.log(uniqueInOrder('AAAABBBCCDAABBB'))
-// ['A','B','C','D','A','B']
+// console.log(uniqueInOrder(182743));
