5f229ee37909342fa01649c408961e79816bf7c2
kellybarber <kelly.james.barber@gmail.com>
2017-11-17 12:30:03 -0500

    comparing my answer to challenge solution

--- unique-in-order.js
+++ unique-in-order.js
@@ -1,10 +1,15 @@
 function uniqueInOrder(iterable) {
-
-  return iterable.toString().split('').map((letter, index) => {
+  return iterable.split('').map((letter, index) => {
     return iterable[index+1] !== letter ? letter : ''
-  }).filter((letter) => { return letter })
-
+  }).filter(letter => { return letter })
 }
 
-console.log(uniqueInOrder('AAAABBBCCDAABBB'))
+// console.log(uniqueInOrder('AAAABBBCCDAABBB'))
 // console.log(uniqueInOrder(182743));
+
+function uniqueInOrder(iterable) {
+  return [].filter.call(iterable, (function (a, i) { return iterable[i - 1] !== a }));
+}
+
+// console.log(uniqueInOrder('AAAABBBCCDAABBB'))
+console.log(uniqueInOrder(988765))
