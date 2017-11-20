0a41cbdb742ac284749910f8e95dade94fd45afc
kellybarber <kelly.james.barber@gmail.com>
2017-11-20 09:40:51 -0500

    problem solved, going to look for more elegant solution

--- count-smiley-faces.js
+++ count-smiley-faces.js
@@ -1,9 +1,21 @@
 function countSmileys(arr) {
+  let validFaces = 0
 
   arr.forEach((face) => {
-    console.log(face);
+    if (face[0] === ':' || face[0] === ';') {
+      if ((face.length === 3) && (face[1] === '' || face[1] === '~' || face[1] === '-')) {
+        if (face[2] === 'D' || face[2] === ')') {
+          validFaces += 1
+        }
+      } else if ((face.length === 2) && (face[1] === 'D' || face[1] === ')')) {
+        validFaces += 1
+      }
+    }
   })
+
+  return validFaces
 
 }
 
+countSmileys([':D',':~)',';~D',':)'])
 countSmileys([':)',':(',':D',':O',':;'])
