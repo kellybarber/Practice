dee33efe8242e4e41275ca9b3607c3b9a63eb8f5
kellybarber <kelly.james.barber@gmail.com>
2017-11-21 14:58:05 -0500

    reviewing with top solutions

--- maximum-length-difference.js
+++ maximum-length-difference.js
@@ -37,3 +37,19 @@ function lengthDifference(a1, a2) {
 a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
 a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
 lengthDifference(a1, a2)
+
+
+// Top Solution
+
+function mxdiflg(a1, a2) {
+  if (a1.length === 0 || a2.length === 0) return -1
+  let l1 = a1.map(str => str.length)
+  let l2 = a2.map(str => str.length)
+  return Math.max(Math.max(...l1) - Math.min(...l2), Math.max(...l2) - Math.min(...l1))
+}
+
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+console.log(mxdiflg(a1, a2))
+
+// Expected 13
