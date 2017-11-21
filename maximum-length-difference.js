aae424f4ef8067b1cd7076eaa2283ead305b3a72
kellybarber <kelly.james.barber@gmail.com>
2017-11-21 14:55:47 -0500

    solution works with main test

--- maximum-length-difference.js
+++ maximum-length-difference.js
@@ -1,10 +1,38 @@
 function lengthDifference(a1, a2) {
+  let longest  = findLongest(a1, a2)
+  let shortest = findShortest(a1, a2)
 
-  console.log(a1)
-  console.log(a2)
+  function findLongest(a1, a2) {
+    let longest = 0
+    function perArray(array) {
+      array.forEach((string) => {
+        if (string.length > longest) {
+          longest = string.length
+        }
+      })
+    }
+    perArray(a1)
+    perArray(a2)
+    return longest
+  }
+
+  function findShortest(a1, a2) {
+    let shortest = a1[0].length
+    function perArray(array) {
+      array.forEach((string) => {
+        if (string.length < shortest) {
+          shortest = string.length
+        }
+      })
+    }
+    perArray(a1)
+    perArray(a2)
+    return shortest
+  }
+
+  return (longest - shortest)
 
 }
-
 
 a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
 a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
