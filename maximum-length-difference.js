5b474ede2ad23fc8544e7d7b1426725f567126c2
kellybarber <kelly.james.barber@gmail.com>
2017-11-21 12:14:28 -0500

    figured out function setup

--- maximum-length-difference.js
+++ maximum-length-difference.js
@@ -1,5 +1,11 @@
 function lengthDifference(a1, a2) {
 
-  
+  console.log(a1)
+  console.log(a2)
 
 }
+
+
+a1 = ["hoqq", "bbllkw", "oox", "ejjuyyy", "plmiis", "xxxzgpsssa", "xxwwkktt", "znnnnfqknaz", "qqquuhii", "dvvvwz"]
+a2 = ["cccooommaaqqoxii", "gggqaffhhh", "tttoowwwmmww"]
+lengthDifference(a1, a2)
