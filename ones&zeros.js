3309b0f544b0b56befb715d4b61a8f1ecd44194f
kellybarber <kelly.james.barber@gmail.com>
2017-11-23 17:51:17 -0500

    new problem adding binary

---
+++ ones&zeros.js
@@ -0,0 +1,10 @@
+function binaryArrayToNumber(arr) {
+
+  arr.reduce((accumulator, currentValue) => {
+    console.log(currentValue);
+  })
+
+}
+
+binaryArrayToNumber([0,0,1,0])
+// 2
--- reg-expressions-practice.js
+++ reg-expressions-practice.js
@@ -3,7 +3,19 @@ let re2 = /abc/
 
 console.log(re1);
 
+
 console.log(/[0123456789]/.test("in 1992"));
 // → true
 console.log(/[0-9]/.test("in 1992"));
 // → true
+
+
+let neighbor = /neighbou?r/;
+console.log(neighbor.test("neighbour"));
+// → true
+console.log(neighbor.test("neighbor"));
+// → true
+
+
+
+// Make Search Bar w/ Regular Expressions //
