8d657160ec770bb8c6237b8f1922d7d61743ad69
kellybarber <kelly.james.barber@gmail.com>
2017-12-04 10:45:45 -0500

    binary calculator completed, doesnt work on site because of bug using reverse function

--- ones&zeros.js
+++ ones&zeros.js
@@ -1,10 +1,33 @@
 function binaryArrayToNumber(arr) {
-
-  arr.reduce((accumulator, currentValue) => {
-    console.log(currentValue);
+  let decimal = 0
+  arr.reverse().forEach((item, index) => {
+    switch (index) {
+      case 0:
+        decimal += item
+        break
+      case 1:
+        decimal += item * 2
+        break
+      case 2:
+        decimal += item * 4
+        break
+      case 3:
+        decimal += item * 8
+        break
+      default: console.log('Not Handled')
+    }
   })
-
+  return decimal
 }
 
 binaryArrayToNumber([0,0,1,0])
 // 2
+binaryArrayToNumber([0,0,0,1])
+// 1
+binaryArrayToNumber([1,1,1,1])
+// 15
+binaryArrayToNumber([0,1,1,0])
+// 6
+binaryArrayToNumber([1,0,1,0])
+// 10
+console.log(binaryArrayToNumber([1,0,1,0]));
